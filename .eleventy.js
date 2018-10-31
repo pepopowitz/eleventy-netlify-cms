98701c2aa722635a6909261d324b34c975ae0cd5
Steven Hicks <steven.j.hicks@gmail.com>
2018-10-30 19:22:57 -0500

    netlify cmis

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,12 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("admin");
 
   eleventyConfig.addCollection("postsReversed", collection => {
     return collection.getFilteredByTag("post").reverse();
   });
-  
+
   return {
     passthroughFileCopy: true
   };
